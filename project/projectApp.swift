1bf72a1752113c83ef78cae4a7de503604d04302
Dujiashan0711 <shandu7652@gmail.com>
2021-12-03 10:26:54 +0800

    Initial Commit

---
+++ project/projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  projectApp.swift
+//  project
+//
+//  Created by 暨大附中２ on 2021/12/3.
+//
+
+import SwiftUI
+
+@main
+struct projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
