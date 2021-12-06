fd6ccaf16dda08041d9def738c1caca607010684
Dujiashan0711 <shandu7652@gmail.com>
2021-12-06 19:39:53 +0800

    原版

---
+++ project/SwiftUIView.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView.swift
+//  project
+//
+//  Created by 暨大附中２ on 2021/12/3.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+    }
+}
